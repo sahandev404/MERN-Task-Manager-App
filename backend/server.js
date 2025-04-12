532a20b22474164efea098592e88e512e82efa12
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-12 19:03:18 +0530

    Add a root route to respond with "Hello World"

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get("/",(req, res) => {
+    res.send("Hello World");
+});
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
