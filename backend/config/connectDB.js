0ce5511f23837f1655c364663250262b88d1f3fb
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-13 17:50:06 +0530

    Refactor connectDB function for improved readability and consistency

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectDB = async ()=>{
-    try{
+const connectDB = async () => {
+    try {
         const connectDB = await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database Connected");        
-    }catch(error){
+        console.log("Database Connected");
+    } catch (error) {
         console.error(error);
         process.exit(1);
     }
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.post("/api/tasks",(req,res)=>{
+app.post("/api/tasks", (req, res) => {
     console.log(req.body);
     res.send("Task Created");
 });
@@ -19,11 +19,11 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(()=>{
+    .then(() => {
         app.listen(PORT, () => {
             console.log(`server is running on port ${PORT}`);
         });
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.error(err);
     });
