3ab60e7275a4d862026ab15ccee6aa409bf79185
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-13 20:38:57 +0530

    add get all tasks route

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,15 @@ app.post("/api/tasks", async (req, res) => {
         const task = await Task.create(req.body);
         res.status(201).json(task);
     } catch (error) {
-        res.status(500).jason({ message: error.message });
+        res.status(500).json({ message: error.message });
+    }
+});
+app.get("/api/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
     }
 });
 
