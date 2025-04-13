3dac4d3dab6436ffcd4b58302e08906212059c91
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-13 18:29:24 +0530

    Refactor task creation endpoint to use async/await and improve middleware organization

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+
 const app = express();
 
+//Middleware
+app.use(express.json()); // Parse JSON bodies
+// const logger = (req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// }
+
+//Routes
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
-app.post("/api/tasks", (req, res) => {
+app.post("/api/tasks", async (req, res) => {
     console.log(req.body);
     res.send("Task Created");
 });
 
-
-app.use(express.json());
-
+// Connect to MongoDB
 const PORT = process.env.PORT || 5000;
-
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
