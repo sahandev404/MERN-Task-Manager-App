716d26368f074189cfb93739d9ec83886ef360ae
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-22 19:31:31 +0530

    feat: Update user registration route and add documentation in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require('../models/userModel');
 
+
+// @desc Register a new user
+// @route POST /api/users/register
+// @access Public
 const registerUser = async (req, res) => {
     res.json({ message: 'User register' })
 }
