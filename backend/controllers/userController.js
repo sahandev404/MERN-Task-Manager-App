86c92083f136d0b81e19ed1ce114660ceb3d47ac
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-26 00:04:54 +0530

    feat: Refactor userController to use express-async-handler for improved error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,28 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
-const registerUser = async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     res.json({ message: 'User register' })
-}
+})
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access Public
-const loginUser = async (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ message: 'User logged in' })
-}
+})
 
 // @desc Get user data
 // @route POST /api/users
 // @access Public
-const getUserData = async (req, res) => {
+const getUserData = asyncHandler(async (req, res) => {
     res.json({ message: 'User data retrieved' })
-}
+})
 
 module.exports = {
     registerUser,
