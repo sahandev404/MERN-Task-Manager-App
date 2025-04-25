0266e9b483ff5d5ecdc031959e1d499d9a50ddeb
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-25 23:28:01 +0530

    feat: Add login and get user data functions to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,26 @@
-const User = require('../models/userModel');
-
-
 // @desc Register a new user
-// @route POST /api/users/register
+// @route POST /api/users
 // @access Public
 const registerUser = async (req, res) => {
     res.json({ message: 'User register' })
 }
 
+// @desc Authenticate a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = async (req, res) => {
+    res.json({ message: 'User logged in' })
+}
+
+// @desc Get user data
+// @route POST /api/users
+// @access Public
+const getUserData = async (req, res) => {
+    res.json({ message: 'User data retrieved' })
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    getUserData,
 }
