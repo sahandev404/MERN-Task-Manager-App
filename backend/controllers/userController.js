d6ca6acb4f40b8c97dc4e899ad06c97fe0a0ee2b
Sahan Devaka <sahandevaka9@gmail.com>
2025-04-26 12:19:56 +0530

    feat: Implement user registration logic with validation and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,42 @@ const User = require('../models/userModel')
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: 'User register' })
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //Check if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            // token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc Authenticate a user
@@ -18,7 +53,7 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 // @desc Get user data
-// @route POST /api/users
+// @route POST /api/users/me
 // @access Public
 const getUserData = asyncHandler(async (req, res) => {
     res.json({ message: 'User data retrieved' })
